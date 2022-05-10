14a1f05c45061686a21a9c437f1b3d348de936bd
KROKODILMALBORO <krokodilmalboro@gmail.com>
2022-05-10 20:24:26 +0700

    Finished Header without link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
-            HEADER
+        <div className='fixed-top shadow'>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+                <div className='container-fluid'>
+                    <span className='navbar-brand'>
+                        INFILM
+                    </span>
+                    <div className='collapse navbar-collapse justify-content-end'>
+                        <ul className='navbar-nav'>
+                            <li className='nav-item mx-2'>
+                                <Link
+                                    className='nav-link'
+                                    to='/'
+                                >
+                                    Главная страница
+                                </Link>
+                            </li>
+                            <li className='nav-item mx-2'>
+                                <Link
+                                    className='nav-link'
+                                    to='/films_search'
+                                >
+                                    Поиск кино
+                                </Link>
+                            </li>
+                            <li className='nav-item mx-2'>
+                                <Link
+                                    className='nav-link'
+                                    to='/persons_search'
+                                >
+                                    Поиск персон
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
