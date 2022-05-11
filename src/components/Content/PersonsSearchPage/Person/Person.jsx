5d4e5266dc3d3f9920ad8f840822d60d94c17a8b
KROKODILMALBORO <krokodilmalboro@gmail.com>
2022-05-11 15:05:43 +0700

    Stylized ~/PersonsSearchPage/Person

--- src/components/Content/PersonsSearchPage/Person/Person.jsx
+++ src/components/Content/PersonsSearchPage/Person/Person.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import linkHover from '../../ui/linkHover/linkHover.module.css'
 
 const Person = (props) => {
     if (!props.person) {
@@ -7,9 +10,27 @@ const Person = (props) => {
     }
 
     return (
-        <div>
-            <span>{props.person.nameRu}</span>
-        </div>
+        <Link
+            className='col-2 text-decoration-none mx-3'
+            type='button'
+            to={`/persons_search/one_person/${props.person.kinopoiskId}`}
+        >
+            <div className='d-flex justify-content-center'>
+                <img
+                    className={`rounded shadow p-2 my-1 ${linkHover.linkPerson}`}
+                    src={props.person.posterUrl}
+                    width='165'
+                    alt='...'
+                />
+            </div>
+            <div className={`rounded-end border-start border-secondary shadow px-2 mx-3 mb-2 ${linkHover.linkPerson}`}>
+                <div>
+                <div className='text-light'>
+                    <span>{props.person.nameRu}</span>
+                </div>
+                </div>
+            </div>
+        </Link>
     )
 }
 
